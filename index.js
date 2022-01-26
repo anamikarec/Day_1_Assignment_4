714107209dba695b3faefb68fd89ea2387e824e5
Anamika <anamikarecsonbhadra@gmail.com>
2022-01-26 22:38:24 +0530

    pagination and all the search param are added

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const http = require('http');
-const {getAllUsers,getUsers,addUser} = require('./app/api/users/');
+const {getAllUsers,getUsers,addUser,getUserById} = require('./app/api/users/');
 
 const server = http.createServer((req,res)=>{
     try{
-        console.log(req.method,req.url);
+        console.log("hi",req.method,req.url);
         const [url, query] = req.url.split("?");
+        console.log("hi2",url,query);
         // BEFORE ? IT WILL BE ADDED TO URL AND AFTER ? IT WILL BE ADDED TO QUERY
         if(url === '/users'){
             if(req.method === 'GET'){
                 const q = new URLSearchParams(`?${query}`);
                 const page = q.get('page') ?? 1;
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(getAllUsers(Number(page))));
-            }
+                const id = q.get('id');
+                console.log(page,q,id,"hello");
+                if(id){
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify(getUserById(Number(id))));
+                }
+                else if(page){
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                     res.end(JSON.stringify(getAllUsers(Number(page))));
+                }
+                }
             else if(req.method === 'POST'){
             console.log(url,query);
             const [name, value] = query.split("=");
@@ -22,11 +31,6 @@ const server = http.createServer((req,res)=>{
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(getAllUsers()));
             }
-        }
-        else if(url.startsWith("/users/")){
-            const index = Number(req.url.split('/')[2]);
-            req.writeHead(201, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(getUsers(index)));
         }
         else{
             throw new Error("query is not understood");
